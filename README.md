4ed6df8f9d28ca01e14d6f3c5e612f6c28bf94fb
Benoit Quartier <benoit.quartier@gmail.com>
2012-11-27 00:29:22 +0100

    small change to make the example more minimal

---
+++ README.md
@@ -0,0 +1,3 @@
+# Experiment with Play 2.1 Enumerator and Concurrent.broadcast
+
+This very small project shows what Concurrent.broadcast does (was "hub" in Play 2.0)
--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -11,7 +11,7 @@ import Execution.Implicits.defaultContext
 object Application extends Controller {
   
   val timeEnum = Streams.getmtime
-  val broadcaster = Concurrent.broadcast[String](timeEnum)    //The Play 2.1 way
+  val broadcaster = Concurrent.broadcast[String](timeEnum)    //The Play 2.1 way; was hub (now depreciated)
 
   val timestep = 2000  // 2 seconds
   
@@ -20,34 +20,23 @@ object Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
   
- 
   //Streams.getmtime is an Enumarator
   //EventSource() return an Enumaratee
   //Streams.getmtime &> EventSource() means: 
   //  passe Streams.getmtime _through_ EventSource and get a modified Enumarator
-  def servereventsindividual = Action {
-    val mtime = Enumerator.generateM{
-      Promise.timeout( {
-        val currentMillis = java.lang.System.currentTimeMillis() / 100
-        Logger.debug(currentMillis toString)
-        Some(currentMillis +" dsec")},
-      timestep, TimeUnit.MILLISECONDS )
-    }
-    Ok.stream(mtime &> EventSource())
+
+    //each client get a different stream of time values
+  def servereventsshared = Action {
+    Ok.stream(timeEnum &> EventSource())
   }
-  
-  // broadcaster._1
+
+  // broadcaster._1 is the magic that broadcast the _same_ output to all client
+  // all clients get the same stream of time values
   def servereventshub = Action {
     implicit val encoder = Comet.CometMessage.stringMessages
     Ok.stream(broadcaster._1 &> EventSource())
   }
-   
-  
-  //gives the same result as servereventsindividual (not intuitive for me, why isn't timeEnum shared between the client?)
-  def servereventsshared = Action {
-    Ok.stream(timeEnum &> EventSource())
-  }
-  
+
 
   object Streams {
     val getmtime = Enumerator.generateM{ 
